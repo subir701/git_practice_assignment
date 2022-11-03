682ce6b301c17099735ca6e96d59b5446c8a7128
Rude00 <subiradhikari181@gmail.com>
2022-11-03 23:45:05 +0530

    checking prime number function

---
+++ prime.js
@@ -0,0 +1,14 @@
+function prime(n){
+    let flag=0;
+    for(let i=2;i<n;i++){
+        if(n%i!=0){
+            flag++
+        }
+    }
+    if(flag==1){
+        return true;
+    }else{
+        return false;
+    }
+}
+prime(13)
